2c6a0c31c470919b1505b2ea4eeba5734e6e83cf
alopes2 <andre.lopes@exxonmobil.com>
2018-06-19 16:04:06 -0300

    adding project

---
+++ Models/Book.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookList.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
